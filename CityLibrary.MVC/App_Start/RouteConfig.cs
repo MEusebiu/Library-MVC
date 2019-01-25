d4de295f9d0e2153e433a8dee78879166895fd29
Eusebiu Mihaes <eusebiu.mihaes@duk-tech.com>
2019-01-25 16:51:44 +0200

    Modified landing page

--- CityLibrary.MVC/App_Start/RouteConfig.cs
+++ CityLibrary.MVC/App_Start/RouteConfig.cs
@@ -12,7 +12,7 @@ namespace CityLibrary.MVC
             routes.MapRoute(
                 name: "default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Author", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
